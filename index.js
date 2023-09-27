6e8fd309ff505f56d619e70c97a8073b27d4b0d4
Dorota Zak <dorotaa.zak@gmail.com>
2023-09-27 10:00:18 +0000

    Added sound functionality and corresponding folders.

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 var numOfButtons = document.querySelectorAll(".drum").length;
 
-function handleClick() {
-  alert("I got clicked!");
-}
+// function handleClick() {
+//   alert("I got clicked!");
+// }
 
 for (i = 0; i < numOfButtons; i++) {
-  document.querySelectorAll("button")[i].addEventListener("click", handleClick);
+  document.querySelectorAll("button")[i].addEventListener("click", function () {
+    //adding sound 'tom-1.mp3' to all of the buttons
+    var audio = new Audio("sounds/tom-1.mp3");
+    audio.play();
+  });
 }
